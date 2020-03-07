2c38483b68ec15f3ec6830a6cd6bd9ce5264c4eb
Ashleigh Newman-Jones <ashnewman-jones@hotmail.co.uk>
2020-03-07 14:46:52 +0000

    Add Cloudflare wrangler with tests

---
+++ Formula/cloudflare-wrangler.rb
@@ -0,0 +1,20 @@
+class CloudflareWrangler < Formula
+  desc "Wrangler is a CLI tool designed for Cloudflare Workers"
+  homepage "https://developers.cloudflare.com/workers/tooling/wrangler/"
+  url "https://github.com/cloudflare/wrangler/archive/v1.8.0.tar.gz"
+  sha256 "ebb31c2549733991823f965025da493bbc2a1644d02668549700793a29df0229"
+  head "https://github.com/cloudflare/wrangler.git"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "#{bin}/wrangler", "--help"
+    assert_match "wrangler " + version.to_s, shell_output("#{bin}/wrangler -V").chomp
+    assert_match /Succesfully created a `wrangler.toml`/, shell_output("#{bin}/wrangler init")
+    assert_predicate testpath/"wrangler.toml", :exist?
+  end
+end
